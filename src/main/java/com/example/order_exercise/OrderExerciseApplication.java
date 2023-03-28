fba002c6cfe52fe7415d4dee73ffb283959ac304
d0np13rr3 <pieter.pauwels13@gmail.com>
2023-03-28 09:24:28 +0200

    Initial commit

---
+++ src/main/java/com/example/order_exercise/OrderExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.order_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderExerciseApplication.class, args);
+    }
+
+}
